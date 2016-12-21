c4be9cc1942f445d88836e4142d808ef6d14f823
vidya <vidyasagarkarnati@yahoo.com>
2016-12-21 00:27:19 -0500

    Fixed Jolokia issues and added basic configuration for thymleaf

--- src/main/java/com/vidya/tools/email/ServletInitializer.java
+++ src/main/java/com/vidya/tools/email/ServletInitializer.java
@@ -2,6 +2,7 @@ package com.vidya.tools.email;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
@@ -9,5 +10,14 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(EmailValidatorApplication.class);
 	}
-
+	
+//	@Bean
+//	public ThymeleafViewResolver viewResolver() {
+//	    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+//	    viewResolver.setViewClass(ThymeleafTilesView.class);
+//	    viewResolver.setTemplateEngine(templateEngine());
+//	    viewResolver.setCharacterEncoding("UTF-8");
+//	    viewResolver.setOrder(0);//ADDED LINE
+//	    return viewResolver;
+//	}
 }
---
+++ src/main/java/com/vidya/tools/email/ui/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.vidya.tools.email.ui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String hello(Model model,
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		model.addAttribute("name", name);
+		return "hello";
+	}
+}
